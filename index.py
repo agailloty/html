dd551aa1b60400c983ed4d450b36be0a6496cf43
agailloty <mr.clerisaxel@hotmail.fr>
2020-11-26 12:31:04 +0100

    Styling main page

--- index.py
+++ index.py
@@ -3,18 +3,23 @@ import markdown, os, json
 files = os.listdir()
 
 
-header = """
-# Index main page
-"""
-
 def gen_markdown():
-    files = os.listdir()
-    links = [f"[{path}]({path})" for path in files]
-    return header + "\n".join(links)
+    files = set(os.listdir()) - {".vscode", "index.py", "css",
+    "index.html", ".git", "REAMDE.md"}
+    links = [f" - [{path}]({path})" for path in files]
+    return " \n ".join(links)
 
 
 html = markdown.markdown(gen_markdown())
 
+body = f"""
+<head> 
+<link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+{html}
+</body>
+"""
 with open("index.html", "w") as file:
-    file.write(html)
+    file.write(body)
 
